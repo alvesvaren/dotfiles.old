6b481f05a0d990954f45c3670e05de17e4d70914
Alve Svarén <alve@hotmail.se>
2020-10-19 22:27:54 +0200

    Added confirming to script and optional package installation for yay

--- script.py
+++ script.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
 
 import os
-from sys import argv
+import subprocess
+from sys import argv, stdout
 from shutil import copyfile
+
+def confirm(msg: str, default: bool = False) -> bool:
+    try:
+        value = input(f"{msg} [{'Y/n' if default else 'y/N'}] ").strip().lower()
+    except KeyboardInterrupt:
+        print()
+        return False
+    return value in ["yes", "y"] or (default and not value)
 
 if __name__ == "__main__":
     offset = 0
@@ -13,6 +22,8 @@ if __name__ == "__main__":
         elif argv[1] == "install":
             offset = -1
             print("Installing files locally")
+            if not confirm("Continue?", True):
+                exit(2)
     except IndexError:
         print(f"Usage: {argv[0]} <install|copy>")
         exit(1)
@@ -22,3 +33,11 @@ if __name__ == "__main__":
             files = (os.environ["HOME"] + "/" + files[0], "./files/" + files[1])[::offset]
             os.makedirs("/".join(files[1].split("/")[:-1]), exist_ok=True)
             copyfile(files[0], files[1])
+    
+    process_stdout = subprocess.Popen("lsb_release -is", shell=True, stdout=subprocess.PIPE).stdout.read()
+    if offset == -1 and os.path.isfile("/usr/bin/yay"):
+        print("Found yay in binary folder...")
+        with open("pkglist") as file:
+            if confirm("Would you like to install packages too?"):
+                os.system("yay --noconfirm -S " + " ".join([i.strip() for i in file.readlines()]))
+
